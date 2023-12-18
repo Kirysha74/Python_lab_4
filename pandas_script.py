3dcf56dff6b82d4ee7169f8ad7529932de60da51
Kirill Trofimov <kaha2015-2016@mail.ru>
2023-12-18 22:34:52 +0400

    Pre-Final Version

--- pandas_script.py
+++ pandas_script.py
@@ -1,10 +1,8 @@
-import pandas as pd
 from datetime import date
+
 import matplotlib.pyplot as plt
+import pandas as pd
 
-filepath = r"C:\Users\user\Desktop\Python\Practice\dataset.csv"
-
-df = pd.read_csv(filepath, delimiter = ";", names = ["Дата", "Курс"])
 
 def delete_invalid(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     df = df.dropna()
@@ -15,45 +13,42 @@ def add_deviation(df: pd.core.frame.DataFrame) -> pd.core.frame.DataFrame:
     df["Median"]=df["Курс"]-df["Курс"].median()
     return df
 
-def median_filtr(df, median):
+def median_filter(df: pd.core.frame.DataFrame, median: float) -> pd.core.frame.DataFrame:
     return df[df["Median"] > median]
 
-def dates(df, date1, date2):
+def filter_by_dates(df: pd.core.frame.DataFrame, eatly_date: str | date, late_date: str | date) -> pd.core.frame.DataFrame:
     date1, date2 = pd.to_datetime(date1), pd.to_datetime(date2)
-    return df[(df["Дата"] > date1) & (df["Дата"] < date2)]
+    return df[(df["Дата"] >= eatly_date) & (df["Дата"] <= late_date)]
 
-def week_course(df, date):
+def course_per_month(df: pd.core.frame.DataFrame, date: date) -> None:
     p = df[(df["Дата"].dt.year == date.year) & (df["Дата"].dt.month == date.month)]
-    p.plot(x = "Дата", y = "Курс")
-    plt.show()
+    p.plot(x = "Дата", y = "Курс", marker='o')
 
 if __name__ == "__main__":
 
-    filepath = r"C:\Users\user\Desktop\Python\Practice\dataset.csv"
-    df = pd.read_csv(filepath, delimiter = ";", names = ["Дата", "Курс"])
+    filepath = r"C:\Users\user\Desktop\Python\Lab_1\Python_lab_1\dataset.csv"
+    df = pd.read_csv(filepath, delimiter = ",", names = ["Дата", "Курс"])
+
     print(df)
-    df["Дата"] = pd.to_datetime(df["Дата"], format = "%Y.%m.%d")
+    df["Дата"] = pd.to_datetime(df["Дата"])
     df = delete_invalid(df)
     print(df)
     df = add_deviation(df)
     print(df)
     print(df[["Курс", "Mean", "Median"]].describe())
-    df = median_filtr(df, 1.0)
+    df = median_filter(df, 1.0)
     print(df)
-    print(dates(df, date(2023, 7, 4), date(2023, 8, 4)))
+    print(filter_by_dates(df, date(2023, 7, 4), date(2023, 8, 4)))
     df_grouped = df.groupby([df['Дата'].dt.year, df['Дата'].dt.month])['Курс'].mean()
-    print(df_grouped.tail(15))
     df_grouped.plot(kind='line', marker='o', figsize=(300, 300))
 
     plt.title('Средний курс по годам и месяцам')
     plt.xlabel('Год и месяц')
     plt.ylabel('Средний курс')
     plt.grid()
-
-# Показать график
     plt.show()
 
-    week_course(df, date(2023, 7, 1))
+    course_per_month(df, date(2022, 2, 1))
 
     
     
